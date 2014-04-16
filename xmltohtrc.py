0dc669eacb5f9bf05cec4f45516c62b9b05f82eb
Jessie Pusateri <jessiepusateri@gmail.com>
2014-04-16 17:09:14 -0400

    near completion on htrc id script

--- xmltohtrc.py
+++ xmltohtrc.py
@@ -2,7 +2,7 @@ import json
 from time import sleep
 from urllib2 import urlopen
 from urllib import quote_plus
-
+import os.path
 import xml.etree.ElementTree as ET
 
 
@@ -10,25 +10,47 @@ def search(title, sleep_time=1):
     """ Queries the HTRC Solr index with a title and returns the resulting metadata.
 Documentation: http://www.hathitrust.org/htrc/solr-api
 """
-    # TODO: Parameterize hostname
-    
-    solr ="http://chinkapin.pti.indiana.edu:9994/solr/meta/select/?q=title:%s" % quote_plus(title)
+    # TODO: Parameterize hostname	
+    solr ="http://chinkapin.pti.indiana.edu:9994/solr/meta/select/?q=title:%s" % quote_plus(title.encode("utf-8"))
     solr += "&wt=json" ## retrieve JSON results
-    print solr
+    # print solr
     # TODO: exception handling
     data = json.load(urlopen(solr))
     if sleep:
         sleep(sleep_time) ## JUST TO MAKE SURE WE ARE THROTTLED
     return data['response']['docs'][0]
 
-if __name__ == '__main__':
-    title = "An account of the Arctic regions"
-    xml_map = search(title)
+def gather_titles():
+
     tree = ET.parse('bhl_darwin_collection.xml')
     root = tree.getroot()
- #   print root.find("TitleID")
     print root.tag
- 
-    print (root.find('.//Result/Title/FullTitle')).text
-   #     print e.attrib
- #   root.xpath = [@TitleID]
+  
+    #finds all the 'Title' tags and print the 'FullTitle' child for each of them
+    for title in root.findall('.//Result/Title/'):
+       return unicode(title.find('FullTitle').text)
+    
+    # find the first 'FullTitle' tag and prints it 
+    
+    #u =  root.find('.//Result/Title/FullTitle')
+    #return u.encode('utf-8')
+    #return unicode(root.find('.//Result/Title/FullTitle'))
+
+def get_htrcid(mytitle):
+    title = "The anatomy and philosophy of expression"
+    myDict = (gather_titles())
+    #myDict = search(title)
+    '''filename = 'test.xml'
+    target = open(filename,'wb')
+    target.write(str(myDict))
+    print myDict
+    '''
+    print myDict.get('id')
+    
+
+
+if __name__ == '__main__':
+    mytitle = "0"
+    get_htrcid(mytitle)
+
+
