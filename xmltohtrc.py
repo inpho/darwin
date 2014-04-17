febe3ff88ceca329542b4f3453c2633dbb3cd673
Jessie Pusateri <jessiepusateri@gmail.com>
2014-04-16 23:55:03 -0400

    Returns HTRCids as a list. Errors persist with titles that have : or / in them.

--- xmltohtrc.py
+++ xmltohtrc.py
@@ -11,11 +11,17 @@ def search(title, sleep_time=1):
 Documentation: http://www.hathitrust.org/htrc/solr-api
 """
     # TODO: Parameterize hostname	
-    solr ="http://chinkapin.pti.indiana.edu:9994/solr/meta/select/?q=title:%s" % quote_plus(title.encode("utf-8"))
+    #print title
+    #print title.encode('utf-8')
+    solr ="http://chinkapin.pti.indiana.edu:9994/solr/meta/select/?q=title:%s" % quote_plus(title)
     solr += "&wt=json" ## retrieve JSON results
-    # print solr
+    #print solr
     # TODO: exception handling
-    data = json.load(urlopen(solr))
+    try:
+        data = json.load(urlopen(solr))
+    except ValueError :
+        print "No result found for " + title
+        return
     if sleep:
         sleep(sleep_time) ## JUST TO MAKE SURE WE ARE THROTTLED
     return data['response']['docs'][0]
@@ -24,33 +30,42 @@ def gather_titles():
 
     tree = ET.parse('bhl_darwin_collection.xml')
     root = tree.getroot()
-    print root.tag
-  
+    #print root.tag
+    #print myDict   
+
     #finds all the 'Title' tags and print the 'FullTitle' child for each of them
+    filename = 'titles.txt'
+    target = open(filename,'wb')  
+
     for title in root.findall('.//Result/Title/'):
-       return unicode(title.find('FullTitle').text)
-    
+    	name = (title.find('FullTitle').text).encode("utf-8")
+        target.write(name)
+        target.write("\n")
+    target.close()
+
     # find the first 'FullTitle' tag and prints it 
     
     #u =  root.find('.//Result/Title/FullTitle')
     #return u.encode('utf-8')
     #return unicode(root.find('.//Result/Title/FullTitle'))
 
-def get_htrcid(mytitle):
+def get_htrcid():
     title = "The anatomy and philosophy of expression"
-    myDict = (gather_titles())
-    #myDict = search(title)
-    '''filename = 'test.xml'
-    target = open(filename,'wb')
-    target.write(str(myDict))
-    print myDict
-    '''
-    print myDict.get('id')
+    myDict = search(title)
+    #filename = 'htrc_ids.txt'
+    importfile = 'titles.txt' 
+    for line in open(importfile):
+        #print line
+        line = line.rstrip('\n')
+        myDict = search(line)
+
+        #print myDict.get('id')
+        #target.write(str(myDict))
+        if myDict:
+            print myDict.get('id')
     
 
 
 if __name__ == '__main__':
-    mytitle = "0"
-    get_htrcid(mytitle)
-
-
+    gather_titles()
+    get_htrcid()
