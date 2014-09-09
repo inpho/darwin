2243908e80989055bc979a14db0fa3065d19b3a8
Jessie Pusateri <jessiepusateri@gmail.com>
2014-09-09 16:49:47 -0400

    removed extraneous heading

--- rython_anystyle.py
+++ rython_anystyle.py
@@ -1,13 +1,9 @@
-#!bin/env ruby
-#encoding: utf-8
-
 import rython
 
 ctx = rython.RubyContext(requires=["rubygems", "anystyle/parser"])
 parser = ctx("Anystyle.parser")
 
-mystring  = """Abbot, Francis Ellingwood. 1872. Truths for the times. Ramsgate,
-Kent Thomas Scott."""
+mystring  = """Abbot, Francis Ellingwood. 1872. Truths for the times. Ramsgate, Kent Thomas Scott."""
 
 parser.parse(mystring)
 
