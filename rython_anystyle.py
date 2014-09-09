7beff2de4aa4607284347db9b2a6426a254b2747
Jessie Pusateri <jessiepusateri@gmail.com>
2014-09-09 17:05:51 -0400

    fixed ASCII error

--- rython_anystyle.py
+++ rython_anystyle.py
@@ -1,10 +1,13 @@
 import rython
 
 ctx = rython.RubyContext(requires=["rubygems", "anystyle/parser"])
+ctx("Encoding.default_internal = 'UTF-8'")
+ctx("Encoding.default_external = 'UTF-8'")
+
 parser = ctx("Anystyle.parser")
 
 mystring  = """Abbot, Francis Ellingwood. 1872. Truths for the times. Ramsgate, Kent Thomas Scott."""
 
-parser.parse(mystring)
+print parser.parse(mystring)
 
 
