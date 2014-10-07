f52ee1b1a8b1b5329872e4f58684c2328a802e5a
Jaimie Murdock <jaimie.murdock@gmail.com>
2014-10-06 23:08:26 -0400

    migrating parser to json format, adding ArgParser. closes #1

---
+++ darparse.py
@@ -0,0 +1,116 @@
+'''Takes a .txt file and passes it through AnySyle to create a new .txt file with the results.
+Choose from cambridge.txt, cambridge.txt, and beagle.txt'''
+
+'''
+Naming conventions: 
+
+For example, if the volume title was 'cambridge'.
+
+INPUT:
+cambridge.txt - text file with a listing of citations. This file must be created
+    before you run the program and it's name must set as the volume_tile for the
+    program to run
+
+OUTPUT:
+cambridge_titles.txt - text file with a listing of the titles that were extracted 
+    from the citations with one title per line
+cambridge_ids.txt - text file with a listing of all the found HTRC ids with an 
+    id per line
+
+'''
+import json
+from time import sleep
+from urllib2 import urlopen
+from urllib import quote_plus
+import os.path
+import xmlrpclib
+
+import rython
+
+from codecs import open
+
+ctx = rython.RubyContext(requires=["rubygems", "anystyle/parser"])
+ctx("Encoding.default_internal = 'UTF-8'")
+ctx("Encoding.default_external = 'UTF-8'")
+
+anystyle = ctx("Anystyle.parser")
+
+'set the title of the input volume you are working on'
+volume_title = 'cambridge'
+
+def parse_citations_from_file(citation_file):
+    return_list = []
+    with open(citation_file, 'r') as readfile:
+        for i, line in enumerate(readfile):
+            line = line.decode('latin-1').encode("utf-8").strip()
+            if line:
+                try:
+                    parsed = anystyle.parse(line)[0]
+                    return_list.append({'original' : line, 'parsed' : parsed})
+                except xmlrpclib.Fault:
+                    return_list.append({'original' : line, 'parsed' : None})
+                    print "error on line %d: %s" % (i, line)
+
+    return return_list
+         
+def search(title, sleep_time=1):
+    """ Queries the HTRC Solr index with a title and returns the resulting metadata.
+    Documentation: http://www.hathitrust.org/htrc/solr-api
+    """
+    # TODO: Parameterize hostname	
+    solr ="http://chinkapin.pti.indiana.edu:9994/solr/meta/select/?q=title:%s" % quote_plus(title)
+    solr += "&wt=json" ## retrieve JSON results
+    # TODO: exception handling
+    try:
+        data = json.load(urlopen(solr))
+    except ValueError :
+        print "No result found for " + title
+        return
+    if sleep:
+        sleep(sleep_time) ## JUST TO MAKE SURE WE ARE THROTTLED
+    try:
+        return data['response']['docs'][0]
+    except IndexError:
+        return None
+
+def populate_htrc(citations):
+    for citation in citations:
+        citation['htrc_id'] = None
+        citation['htrc_md'] = None
+        if citation['parsed']:
+            title = citation['parsed'].get('title')
+            if title:
+                title = title.replace("/", "")
+                title = title.replace(":", "")
+                title = title.replace("[", "")
+                title = title.replace("]", "")
+                htrc_md = search(title.encode('utf-8'))
+            
+                if htrc_md:
+                    citation['htrc_md'] = htrc_md
+                    citation['htrc_id'] = htrc_md.get('id')
+
+    return citations
+
+def extant_file(x):
+    """
+    'Type' for argparse - checks that file exists but does not open.
+    """
+    if not os.path.isfile(x):
+        raise argparse.ArgumentError("%s does not exist" % x)
+    return x
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-o', dest='output', help="output file", default='out.json')
+    parser.add_argument('citation_file', help="citation file", 
+        type=extant_file)
+    args = parser.parse_args()
+
+    citations = parse_citations_from_file(args.citation_file)
+    citations = populate_htrc(citations)
+
+    with open(args.output, 'wb') as output_file:
+        json.dump(citations, output_file)
+
